c6bff6e5799b9d9a33574a8d64f1a9dbdd21e053
JoseWRPereira <josewrpereira@gmail.com>
2021-09-03 12:52:26 -0300

    Atualização de makefile das aulas 1 e 2 e inclusão das imagens gif na aula 2 bem como o código

---
+++ aula02_botaoLED.X/main.c
@@ -0,0 +1,43 @@
+/*
+ * File:   main.c
+ * Author: josewrpereira
+ *
+ * Created on 03 September 2021, 11:45
+ * 
+ * IDE:                 MPLAB X IDE v5.45
+ * Compiler:            XC8 v2.31
+ * Operating System:    Debian GNU/Linux bullseye/sid
+ * Kernel:              Linux 5.10.0-5-amd64
+ * Architecture:        x86-64
+ * 
+ * Objetivo: 
+ *      Acionar o LED em função do botão
+ * 
+ * Pinos    |nº     |Conexão
+ *  VDD     |11,32  | Alimentação (Vcc/+5V)
+ *  VSS     |12,31  | Alimentação (GND/0V)
+ *  RD0     |19     | LED   (source)
+ *  RD3     |22     | BOTAO (pullDown)
+ */
+
+
+#include "config.h"             // Inclui diretivas de configuração do uC
+#include <xc.h>                 // Inclui biblioteca padrão do compilador XC8 
+                                // para microcontroladores Microchip.
+
+#define LED     PORTDbits.RD0   // Definição ou "apelido", para o pino RD0
+#define BOTAO   PORTDbits.RD3   // Definição ou "apelido", para o pino RD3
+
+void main(void)                 // Função principal = main.
+{                               // Início do escopo da função main.
+    LED = 0;                    // Inicia LED apagado.
+    TRISDbits.TRISD0 = 0;       // Configura RD0 como Saída.
+    TRISDbits.TRISD3 = 1;       // Configura RD3 como Entrada;
+                                // Não precisa inicialização do valor do pino.
+
+    while( 1 )                  // Laço de repetição infinito.
+    {                           // Inicio do escopo do laço de repetição.
+        LED = BOTAO;            // O LED recebe o valor do BOTAO.
+    }                           // Fim do escopo do laço de repetição.
+    return;                     // Caracteriza main como uma função com retorno vazio.
+}                               // Fim do escopo da função main.
