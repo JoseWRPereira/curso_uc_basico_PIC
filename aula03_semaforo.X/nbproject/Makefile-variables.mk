8d7f6cd21f46ccf90bc3509f967afb4caa6216a6
JoseWRPereira <josewrpereira@gmail.com>
2021-09-03 16:46:35 -0300

    Inclusao de arquivos da aula03-semaforo. Ainda não testado

---
+++ aula03_semaforo.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=aula03_semaforo.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/aula03_semaforo.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=aula03semaforo.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/aula03semaforo.x.tar
