2afe49d48cca6bc6bfff6775a6ba8cd3edb5323c
JoseWRPereira <josewrpereira@tutanota.com>
2021-12-14 21:49:52 -0300

    Inclusão de arquivos do projeto da aula08 display com 4 vias de barramento de dados

---
+++ aula08_lcd4vias.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=aula08_lcd4vias.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/aula08_lcd4vias.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=aula08lcd4vias.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/aula08lcd4vias.x.tar
